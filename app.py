e3c0325dedbb7d42a1397d02e911199e1c06dacd
alexjhoel <107956762+alexjhoel369@users.noreply.github.com>
2024-10-16 16:13:10 -0400

    Cambio realizado

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Página de inicio
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Página "Quiénes Somos"
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Página de Servicios
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+# Página de Noticias
+@app.route('/news')
+def news():
+    return render_template('news.html')
+
+# Página de Contacto
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    coordenadas = (-16.48132, -68.18718) 
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        email = request.form['email']
+        mensaje = request.form['mensaje']
+    return render_template('contact.html', coordenadas=coordenadas)
+
+if __name__ == '__main__':
+    app.run(debug=True)
